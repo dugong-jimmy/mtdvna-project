fb85bdac7dc7117898eacf51f179fc298e7bf070
James Blair <jblairdugong@gmail.com>
2017-11-12 20:39:51 +0000

    Added basic scene with some movement controls

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (h * rb2d.velocity.x < maxSpeed)
+        {
+            rb2d.AddForce(Vector2.right * h * moveForce);
+        }
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+    }
+}
